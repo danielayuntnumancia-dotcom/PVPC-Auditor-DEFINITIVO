35351febf3ea7dd68f74b4c5866a06edb2f6aeb7
danielayuntnumancia-dotcom <daniel.ayuntnumancia@gmail.com>
2026-07-24 18:09:32 +0200

    Android app

---
+++ android/app/src/main/java/es/numancia/pvpcauditor/MainActivity.java
@@ -0,0 +1,5 @@
+package es.numancia.pvpcauditor;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'es.numancia.pvpcauditor',
+  appName: 'PVPC Auditor',
+  webDir: 'dist'
+};
+
+export default config;
